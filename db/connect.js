2333ca27abb1eb3fe53456e4010ed2ec34a7be52
Jade Miller <1jaremil1@gmail.com>
2025-06-20 01:49:06 -0600

    q

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,50 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const MongoClient = require('mongodb').MongoClient;
+
+// let _db;
+
+// const initDb = (callback) => {
+//   if (_db) {
+//     console.log('Db is already initialized!');
+//     return callback(null, _db);
+//   }
+//   MongoClient.connect(process.env.MONGODB_URI)
+//     .then((client) => {
+//       _db = client;
+//       callback(null, _db);
+//     })
+//     .catch((err) => {
+//       callback(err);
+//     });
+// };
+
+// const getDb = () => {
+//   if (!_db) {
+//     throw Error('Db not initialized');
+//   }
+//   return _db;
+// };
+
+// module.exports = {
+//   initDb,
+//   getDb
+// };
+
+const { MongoClient } = require('mongodb');
 
 let _db;
 
 const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error('MONGODB_URI is not defined in environment variables');
   }
-  MongoClient.connect(process.env.MONGODB_URI)
+
+  MongoClient.connect(uri)
     .then((client) => {
-      _db = client;
-      callback(null, _db);
+      _db = client.db(); // or specify db name here: client.db('your-db')
+      callback();
     })
     .catch((err) => {
       callback(err);
@@ -21,7 +53,7 @@ const initDb = (callback) => {
 
 const getDb = () => {
   if (!_db) {
-    throw Error('Db not initialized');
+    throw new Error('Database not initialized');
   }
   return _db;
 };
--- server.js
+++ server.js
@@ -1,32 +1,4 @@
-// require('dotenv').config();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongodb = require('./db/connect');
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// app
-//   .use(bodyParser.json())
-//   .use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     next();
-//   })
-//   .use('/', require('./routes'));
-
-// mongodb.initDb((err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     app.listen(port);
-//     console.log(`Connected to DB and listening on ${port}`);
-//   }
-// });
-
-// const configFile = process.env.CONFIG_FILE_NAME;
-// console.log(configFile);
-
-require('dotenv').config(); // Move this to the top
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
@@ -43,13 +15,12 @@ app
 
 mongodb.initDb((err) => {
   if (err) {
-    console.error('Failed to connect to DB', err);
+    console.log(err);
   } else {
-    app.listen(port, '0.0.0.0', () => {
-      console.log(`Connected to DB and listening on port ${port}`);
-    });
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
   }
 });
 
 const configFile = process.env.CONFIG_FILE_NAME;
-console.log('Config File:', configFile);
+console.log(configFile);
