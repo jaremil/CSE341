48365b99f505484c80e592d7c470ca239b490f73
Jade Miller <1jaremil1@gmail.com>
2025-06-19 21:42:45 -0600

    b

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const router = express.Router();
+const routes = require('express') .Router();
 
-router.use('/contacts', require('./contacts'));
+const myController = require("../controllers")
 
-module.exports = router;
+// original code in Professor Birch's expample. working code in ../controllers/index.js as shown above
+// routes.get('/', (req, res, next) => {
+//     res.json('Jade Furtado')
+// });
+
+/**
+ * Parameters
+ * hello string
+ */
+
+// GET all, GET by id, PUT, Post, and DELETE
+routes.get('/contacts', myController.awesomeFunction);
+routes.put('/contacts', (req, res, next)=>{console.log('hello')});
+routes.post('/contacts', (req, res, next)=>{console.log('hello')});
+routes.delete('/contacts', (req, res, next)=>{console.log('hello')});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 8080;
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+app.use('/', require('./routes'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+app.listen(port,() => {
+    console.log(`Server is running at port ${port}.`);
 });
