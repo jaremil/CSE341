1f91f32573e679a03a935935a73b57b140cd1b70
Jade Miller <1jaremil1@gmail.com>
2025-06-20 01:16:32 -0600

    v

--- server.js
+++ server.js
@@ -21,3 +21,7 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+require('dotenv').config();
+const configFile = process.env.CONFIG_FILE_NAME;
+console.log(configFile);
