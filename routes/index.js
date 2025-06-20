b766a28b679adacc55dad3f673f4276f160485b4
Jade Miller <1jaremil1@gmail.com>
2025-06-20 01:39:49 -0600

    h

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
-const routes = require('express') .Router();
+// const routes = require('express') .Router();
+// const express = require('express');
+// const router = express.Router();
+
+// const myController = require("../controllers")
+
+// // original code in Professor Birch's expample. working code in ../controllers/index.js as shown above
+// // routes.get('/', (req, res, next) => {
+// //     res.json('Jade Furtado')
+// // });
+
+// /**
+//  * Parameters
+//  * hello string
+//  */
+
+// // GET all, GET by id, PUT, Post, and DELETE
+// routes.get('/contacts', myController.awesomeFunction);
+// routes.put('/contacts', (req, res, next)=>{console.log('hello')});
+// routes.post('/contacts', (req, res, next)=>{console.log('hello')});
+// routes.delete('/contacts', (req, res, next)=>{console.log('hello')});
+// router.use('/contacts', require('./contacts'));
+
+// module.exports = routes;
+
 const express = require('express');
 const router = express.Router();
 
-const myController = require("../controllers")
-
-// original code in Professor Birch's expample. working code in ../controllers/index.js as shown above
-// routes.get('/', (req, res, next) => {
-//     res.json('Jade Furtado')
-// });
-
-/**
- * Parameters
- * hello string
- */
-
-// GET all, GET by id, PUT, Post, and DELETE
-routes.get('/contacts', myController.awesomeFunction);
-routes.put('/contacts', (req, res, next)=>{console.log('hello')});
-routes.post('/contacts', (req, res, next)=>{console.log('hello')});
-routes.delete('/contacts', (req, res, next)=>{console.log('hello')});
 router.use('/contacts', require('./contacts'));
 
-module.exports = routes;
+module.exports = router;
