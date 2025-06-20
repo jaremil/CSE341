300004d60d4392f6ba2b91fd42af4cf044209194
Jade Miller <1jaremil1@gmail.com>
2025-06-20 01:45:18 -0600

    u

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongodb = require('./db/connect');
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// app
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
+//   .use('/', require('./routes'));
+
+// mongodb.initDb((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port);
+//     console.log(`Connected to DB and listening on ${port}`);
+//   }
+// });
+
+// const configFile = process.env.CONFIG_FILE_NAME;
+// console.log(configFile);
+
+require('dotenv').config(); // Move this to the top
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
@@ -15,12 +43,13 @@ app
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.error('Failed to connect to DB', err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Connected to DB and listening on port ${port}`);
+    });
   }
 });
 
 const configFile = process.env.CONFIG_FILE_NAME;
-console.log(configFile);
+console.log('Config File:', configFile);
